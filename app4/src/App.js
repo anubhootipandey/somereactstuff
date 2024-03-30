48ececd3fe7c2a641a70da246d6b4fe2b1c1e1f7
anubhootipandey <anubhootipandey05@gmail.com>
2024-03-30 11:24:29 +0530

    sessionstorage-and-localstorage

---
+++ app4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StorageExample from './components/StorageExample';
+
+function App() {
+  return (
+    <div className="App">
+      <StorageExample />
+    </div>
+  );
+}
+
+export default App;
