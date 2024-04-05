668610462637b980e28a1248cdf52121ad5faa12
anubhootipandey <anubhootipandey05@gmail.com>
2024-04-05 11:22:27 +0530

    react-concepts

---
+++ interview-coding-question/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Country from './components/Country'
+// import { CheckBox } from '@mui/icons-material'
+
+const App = () => {
+  return (
+    <div>
+      <Country />
+      {/* <CheckBox /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ react-projects/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import RecipeFinder from './components/RecipeFinder';
+// import GithubuserSearch from './components/GithubuserSearch';
+// import ShoppingList from './components/ShoppingList';
+// import Quote from './components/Quote';
+// import StorageExample from './components/StorageExample';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <StorageExample /> */}
+      {/* <Quote /> */}
+      {/* <ShoppingList /> */}
+      {/* <GithubuserSearch /> */}
+      <RecipeFinder />
+    </div>
+  );
+}
+
+export default App;
