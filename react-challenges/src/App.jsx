de21259dc00836f4752038f5ee8bf5744949e935
anubhootipandey <anubhootipandey05@gmail.com>
2024-04-11 11:07:36 +0530

    react-challenges

---
+++ react-challenges/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ArrayToList from './challenges/ArrayToList';
+
+function App() {
+  return (
+    <>
+      <h1>React Challenges</h1>
+      <ArrayToList />
+    </>
+  )
+}
+
+export default App;
