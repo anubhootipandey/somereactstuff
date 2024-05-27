ec1fbdaa758a59b8cbd8f8f8a0303f65de368baa
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-27 22:22:13 +0530

    react tabbing

---
+++ React-Tutorial/tabbing/src/Data/tabs.js
@@ -0,0 +1,14 @@
+export const tabs = [
+    {
+        'title': "Vision",
+        'description': "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    },
+    {
+        'title': "Mission",
+        'description': "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    },
+    {
+        'title': "Values",
+        'description': "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    },
+]
