0c0bfe3a60ab759ff6080c9ef81982b934bd05f0
anubhootipandey <anubhootipandey05@gmail.com>
2025-05-29 22:51:07 +0530

    DebounceCustomHook

--- interview-coding-question/src/App.js
+++ interview-coding-question/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
 // import Comment from './components/Comment'
-// import DebounceCustomHook from './components/DebounceCustomHook'
+import DebounceCustomHook from './components/DebounceCustomHook'
 // import SearchFilter from './components/SearchFilter'
 // import List from './components/List'
 // import Country from './components/Country'
@@ -14,9 +14,9 @@ const App = () => {
       {/* <CheckBox /> */}
       {/* <List /> */}
       {/* <SearchFilter /> */}
-      {/* <DebounceCustomHook /> */}
+      <DebounceCustomHook />
       {/* <Comment /> */}
-      <Counter />
+      {/* <Counter /> */}
     </div>
   )
 }
