0c4c6b00d11edab45ec6fd9fd2078008fe3d7316
anubhootipandey <anubhootipandey05@gmail.com>
2025-05-31 22:39:42 +0530

    new changes

--- interview-coding-question/src/App.js
+++ interview-coding-question/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 // import Counter from './components/Counter'
 // import Comment from './components/Comment'
 // import DebounceCustomHook from './components/DebounceCustomHook'
-import SearchFilter from './components/SearchFilter'
-// import List from './components/List'
+// import SearchFilter from './components/SearchFilter'
+import List from './components/List'
 // import Country from './components/Country'
 // import { CheckBox } from '@mui/icons-material'
 
@@ -12,8 +12,8 @@ const App = () => {
     <div>
       {/* <Country /> */}
       {/* <CheckBox /> */}
-      {/* <List /> */}
-      <SearchFilter />
+      <List />
+      {/* <SearchFilter /> */}
       {/* <DebounceCustomHook /> */}
       {/* <Comment /> */}
       {/* <Counter /> */}
